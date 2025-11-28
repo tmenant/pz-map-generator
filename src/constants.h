3b4a6e2b678131c7655690b96bebdc71909e0083
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-11-28 22:59:49 +0100

    create constants.h

---
+++ src/constants.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+
+namespace constants
+{
+    constexpr int MIN_LAYER = -32;
+    constexpr int MAX_LAYER = -32;
+
+    constexpr int BLOCK_SIZE_IN_SQUARE = 8;
+    constexpr int SQUARE_PER_BLOCK = BLOCK_SIZE_IN_SQUARE * BLOCK_SIZE_IN_SQUARE;
+
+    constexpr int CELL_SIZE_IN_BLOCKS = 32;
+    constexpr int BLOCKS_PER_CELL = CELL_SIZE_IN_BLOCKS * CELL_SIZE_IN_BLOCKS;
+
+    constexpr char LINE_END = 0xA;
+
+    constexpr std::string_view LOTHEADER_EXT = ".lotheader";
+    constexpr std::string_view LOTPACK_EXT = ".lotpack";
+    constexpr std::string_view PACK_EXT = ".pack";
+
+    constexpr std::string_view MAGIC_LOTHEADER = "LOTH";
+    constexpr std::string_view MAGIC_LOTPACK = "LOTP";
+    constexpr std::string_view MAGIC_LOTPACK = "PZPK";
+    constexpr std::string_view MAGIC_TILEDEF = "tdef";
+}
